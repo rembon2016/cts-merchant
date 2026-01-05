28f066439451f14ef1b0abf134aa099d5c6a4a28
Ade Yusuf <adeyusuf005@gmail.com>
2026-01-05 09:46:15 +0700

    UPDATE: fix minor bugs

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-f5cea9ed'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ithn1jbj7l"
+    "revision": "0.jn8eiqfdq58"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
