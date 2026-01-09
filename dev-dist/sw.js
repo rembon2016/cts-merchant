47803cadbdc2f5d3b8c06e6db093b34585bea726
Ade Yusuf <adeyusuf005@gmail.com>
2026-01-09 14:55:28 +0700

    UPDATE: scrool size in modal box

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-f5cea9ed'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.abounuot15g"
+    "revision": "0.ljrhrodtquo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
