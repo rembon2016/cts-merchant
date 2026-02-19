ba57247af7a8b3ed5ef0cd01f73e2381a36745b8
Ade Yusuf <adeyusuf005@gmail.com>
2026-02-19 15:57:37 +0700

    FIX: error build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.3tj4d09o628"
+    "revision": "0.54viqpotj4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
